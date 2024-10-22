61e00930d2bbed1e9c713d84cfde5b01c86ec06a
alex <code-signing@actorpus.com>
2024-10-22 21:54:04 +0100

    Added Event class and the two lists to the event handler fixed spelling mistakes in eve

---
+++ game/core/src/main/java/grymV2/game/server/Event.java
@@ -0,0 +1,16 @@
+package grymV2.game;
+
+abstract class Event {
+    private const float startTime;
+    private float runEvery;
+
+    public float getRunEvery() {
+        return runEvery;
+    }
+
+    public float getStartTime() {
+        return startTime;
+    }
+
+    abstract public boolean tick(float time)
+}
--- game/core/src/main/java/grymV2/game/server/EventHandler.java
+++ game/core/src/main/java/grymV2/game/server/EventHandler.java
@@ -1,6 +1,12 @@
 package grymV2.game;
 
+import java.util.ArrayList;
+import grymv2.game.Event;
+
 class EventHandler {
+    private ArrayList<Event> eventQue;
+    private ArrayList<Event> eventPool;
+
     public void tick(int time) {
 
     }
