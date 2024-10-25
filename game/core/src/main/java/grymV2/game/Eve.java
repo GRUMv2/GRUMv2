bcbc9e901d4d24c34025df71fecc74a47a1ff4ee
Alex <code-signing@actorpus.com>
2024-10-25 17:30:51 +0100

    introducing-logging

--- game/core/src/main/java/grymV2/game/Eve.java
+++ game/core/src/main/java/grymV2/game/Eve.java
@@ -1,12 +1,17 @@
 package grymV2.game;
 
-import grymV2.game.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import grymV2.game.server.Abel;
 
 class Eve {
+    private static final Logger logger = LogManager.getLogger(Eve.class);
     private Abel gameThread;
 
-    private void Eve() {
+    public Eve() {
+        logger.debug("Creating and starting gameserver (Abel)");
+
         gameThread = new Abel();
         gameThread.start();
     }
