256e81c18bfa8b061cbd59affaccbcb0ca2dd5ca
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-03 19:59:29 +0000

    client(input -> mouse): basic mouse position tracking

--- game/core/src/main/java/grymV2/game/client/input/AbstractInputHandler.java
+++ game/core/src/main/java/grymV2/game/client/input/AbstractInputHandler.java
@@ -4,5 +4,15 @@ package grymV2.game.client.input;
  * InputHandler
  */
 public abstract class AbstractInputHandler {
+    int mouseX;
+    int mouseY;
 
+    public void setMousePos(int mouseX, int mouseY) {
+        this.mouseX = mouseX;
+        this.mouseY = mouseY;
+    }
+
+    public void setScroll(float scrollX, float scrollY) {
+        // not implemented
+    }
 }
