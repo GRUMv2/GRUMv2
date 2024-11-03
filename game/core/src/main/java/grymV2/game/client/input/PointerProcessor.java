2811bd5ec161651a96d261a5600b32e23429b88c
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-03 19:59:29 +0000

    client(input -> mouse): basic mouse position tracking

--- game/core/src/main/java/grymV2/game/client/input/PointerProcessor.java
+++ game/core/src/main/java/grymV2/game/client/input/PointerProcessor.java
@@ -4,7 +4,19 @@ package grymV2.game.client.input;
  * PointerProcessor - InputProcessor for mouse/touch events
  */
 public class PointerProcessor extends AbstractInputProcessor {
+    private AbstractInputHandler handler;
     public PointerProcessor(AbstractInputHandler handler) {
         super(handler);
+        this.handler = this.getHandler();
+    }
+
+    public boolean mouseMoved(int screenX, int screenY) {
+        this.handler.setMousePos(screenX, screenY);
+        return true;
+    }
+
+    public boolean scrolled(float amountX, float amountY) {
+        this.handler.setScroll(amountX, amountY);
+        return false;
     }
 }
