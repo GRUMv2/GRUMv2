de944ab133cb6bccbe29183c169985644bff43f8
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-06 22:41:13 +0000

    loader(Adam, Eve, Settings): basic init

--- game/core/src/main/java/grymV2/game/Eve.java
+++ game/core/src/main/java/grymV2/game/Eve.java
@@ -4,21 +4,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import grymV2.game.server.Abel;
+import grymV2.game.grid.Grid;
 
 class Eve {
     private static final Logger logger = LogManager.getLogger(Eve.class);
-    private Abel gameThread;
+    private Abel serverThreaded;
 
-    public Eve(Adam game, Settings settings) {
+    public Eve(Adam game) {
         logger.debug("Creating and starting gameserver (Abel)");
 
-        gameThread = new Abel(120);
-        gameThread.start();
-        //client = new Cain(game);
+        Grid grid = new Grid(game.settings.getGameSize());
+
+        serverThreaded = new Abel(120, grid);
+        serverThreaded.start();
+        game.serverThreaded = serverThreaded;
+        //client = new Cain(game, grid);
+        //game.client = client;
     }
 
     public void dispose() {
-        gameThread.endGame();
-
+        serverThreaded.endGame();
     }
 }
