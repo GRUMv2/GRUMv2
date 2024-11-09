36ad36741795faab5baed033110d81ba96334133
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-09 06:15:07 +0000

    loader(Adam, Eve): Initialise Cain

--- game/core/src/main/java/grymV2/game/Eve.java
+++ game/core/src/main/java/grymV2/game/Eve.java
@@ -2,32 +2,30 @@ package grymV2.game;
 
 import java.io.FileNotFoundException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import grymV2.game.server.Abel;
+import grymV2.game.client.Cain;
 import grymV2.game.grid.Grid;
 
 class Eve {
-    private static final Logger logger = LogManager.getLogger(Eve.class);
     private Abel serverThreaded;
+    private Cain client;
 
     public Eve(Adam game) {
-        logger.debug("Creating and starting gameserver (Abel)");
+        GameLogger.debug(Eve.class, "Creating and starting gameserver (Abel)");
 
         Grid grid = new Grid(game.settings.getGameSize());
         try {
             grid = Map.genMap(grid.getSize());
         } catch (FileNotFoundException | Map.MapException e) {
-            logger.error("Cannot load map: " + e.toString());
+            GameLogger.error(Eve.class, "Cannot load map: " + e.toString());
             System.exit(2);
         }
 
         serverThreaded = new Abel(120 /* , grid */);
         serverThreaded.start();
         game.serverThreaded = serverThreaded;
-        //client = new Cain(game, grid);
-        //game.client = client;
+        client = new Cain(game, grid);
+        game.client = client;
     }
 
     private void loadMap(Grid grid) {
