eec2dad937d35e53127807ee68daf0b7375b34c2
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-09 06:04:21 +0000

    client(input processors): Fix accessing correct inputhandler

--- game/core/src/main/java/grymV2/game/client/input/PointerProcessor.java
+++ game/core/src/main/java/grymV2/game/client/input/PointerProcessor.java
@@ -4,27 +4,25 @@ package grymV2.game.client.input;
  * PointerProcessor - InputProcessor for mouse/touch events
  */
 public class PointerProcessor extends AbstractInputProcessor {
-    private AbstractInputHandler handler;
     public PointerProcessor(AbstractInputHandler handler) {
         super(handler);
-        this.handler = this.getHandler();
     }
 
     public boolean mouseMoved(int screenX, int screenY) {
-        this.handler.setMousePos(screenX, screenY);
+        this.getHandler().setMousePos(screenX, screenY);
         return true;
     }
 
     public boolean scrolled(float amountX, float amountY) {
-        this.handler.setScroll(amountX, amountY);
+        this.getHandler().setScroll(amountX, amountY);
         return false;
     }
 
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return this.handler.click(button, true);
+        return this.getHandler().click(button, true);
     }
 
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return this.handler.click(button, false);
+        return this.getHandler().click(button, false);
     }
 }
