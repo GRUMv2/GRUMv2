ef6428da24cdfc6d68c9698bc281110237fbbd94
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-09 17:43:46 +0000

    client(screens): Pause screen

--- game/core/src/main/java/grymV2/game/client/CainsLeftKnee.java
+++ game/core/src/main/java/grymV2/game/client/CainsLeftKnee.java
@@ -2,38 +2,49 @@ package grymV2.game.client;
 
 import grymV2.game.grid.Grid;
 
+import grymV2.game.client.ui.Layout;
+import grymV2.game.client.ui.TextBox;
+
 /**
  * Cain's Left Knee - Pause Screen
  */
 public class CainsLeftKnee extends AbstractGameScreen {
+
+    private Layout<TextBox> pauseLayout;
+    private TextBox pauseText;
+
     public CainsLeftKnee(Cain manager, Grid grid) {
         super(manager, grid);
+        this.pauseLayout = new Layout<TextBox>(viewport.getWorldWidth() * 0.7f, viewport.getWorldHeight());
+        this.pauseLayout.centreLayout(viewport);
+        this.pauseText = new TextBox(this.font);
+        this.pauseText.setText("Game Paused");
+        this.pauseText.setTextColor(this.manager.game.settings.getUIForeground());
+        this.pauseText.setColor(this.manager.game.settings.getUIBackground());
+        this.pauseLayout.add(this.pauseText);
     }
 
     @Override
     public void input() {
-        // TODO Auto-generated method stub
+        this.manager.handleInput();
     }
 
     @Override
     public void logic() {
-        // TODO Auto-generated method stub
     }
 
     @Override
     public void draw() {
-        // TODO Auto-generated method stub
-
+        this.viewport.apply();
+        this.pauseLayout.draw(this.shapeRenderer, this.batch, this.viewport.getCamera());
     }
 
-
     @Override
-    public void pause() {
-        this.manager.pause();
+    public void resume() {
+        this.manager.resume();
     }
 
     @Override
     public void dispose() {
-        // TODO Auto-generated method stub
     }
 }
