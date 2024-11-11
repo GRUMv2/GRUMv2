e492f3f02c3aad0c7782bf684cd95f84d991481c
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-11 02:27:55 +0000

    loader(grid): "TODO: Literally anything that isn't this"

--- game/core/src/main/java/grymV2/game/grid/Grid.java
+++ game/core/src/main/java/grymV2/game/grid/Grid.java
@@ -3,6 +3,13 @@ package grymV2.game.grid;
 import java.util.ArrayList;
 
 import grymV2.game.config.MapSizes;
+import grymV2.game.world.BackgroundGameObject;
+import grymV2.game.world.Building;
+import grymV2.game.world.ForegroundGameObject;
+import grymV2.game.world.GameObject;
+import grymV2.game.world.Lake;
+import grymV2.game.world.Placeable;
+import grymV2.game.world.Road;
 
 public class Grid {
     // If the user is defining grid sizes greater than 127 in a given direction
@@ -47,26 +54,82 @@ public class Grid {
         return this.grid.get(x).get(y);
     }
 
-    public Boolean isPlaceable(byte x, byte y) {
-        if (this.grid.get(x).get(y)
-                .getLayer(GridLayers.FOREGROUND)
-                .getContent() == ForegroundTileContent.AGNOSTIC) {
+    public boolean isPlaceable(byte x, byte y) {
+        Tile t = this.getTile(x, y);
+        if (t.isDevelopable() && t.isEmpty(GridLayers.FOREGROUND)) {
             return true;
         }
         return false;
     }
 
-    public Boolean isEdge(byte x, byte y) {
+    public boolean isPlaceable(Tile t) {
+        byte x = t.getCoordX();
+        byte y = t.getCoordY();
+        if (this.isTile(x, y)) {
+            return this.isPlaceable(x, y);
+        }
+        return false;
+    }
+
+    public boolean isEdge(byte x, byte y) {
         if (x == 0 || y == 0 || x == this.x || y == this.y) {
             return true;
         }
         return false;
     }
 
-    public Boolean isTile(byte x, byte y) {
+    public boolean isTile(byte x, byte y) {
         if (x >= 0 && x <= this.x && y >= 0 && y <= this.y) {
             return true;
         }
         return  false;
+    }
+
+    public boolean updateTile(byte x, byte y, GameObject newContent, GridLayers layer) {
+        Tile t = this.getTile(x, y);
+        switch (layer) {
+            case BACKGROUND:
+                if (!(newContent instanceof BackgroundGameObject)) {
+                    return false;
+                }
+                // Currently sort of redundant because all BACKGROUND tiles
+                // are by default immutable.
+                if (!t.isMutable(layer)) {
+                    return false;
+                }
+                break;
+
+            case FOREGROUND:
+                if (!(newContent instanceof ForegroundGameObject)) {
+                    return false;
+                }
+                if (t.isEmpty(GridLayers.BACKGROUND)) {
+                    // Foreground objects require a background tile
+                    return false;
+                } else if (!t.isMutable(layer)) {
+                    return false;
+                } else if (t.getLayer(layer) instanceof Building) {
+                    // Doesn't allow for meteors
+                    return false;
+                } else if (newContent instanceof Placeable && ! this.isPlaceable(t)) {
+                    return false;
+                }
+                if (newContent instanceof Road && t.getLayer(GridLayers.BACKGROUND) instanceof Lake) {
+                    return false;
+                }
+                break;
+
+            case DYNAMIC:
+            case PERSONAL:
+            default:
+                return false;
+        }
+
+        //if (newContent instanceof Road && t.getLayer(GridLayers.BACKGROUND) instanceof River) {
+        //    newContent = (Road) newContent;
+        //    newContent.setTexture(TextureMap.BRIDGE);
+        //}
+        t.setLayer(layer, newContent);
+        return true;
     }
 }
