8191a081bc2f3b3377bf0d8111b7e7c127cdf097
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-11-11 00:41:40 +0000

    world: Developable interface

--- game/core/src/main/java/grymV2/game/world/Terrain.java
+++ game/core/src/main/java/grymV2/game/world/Terrain.java
@@ -5,7 +5,7 @@ import grymV2.game.config.TextureMap;
 /**
  * Terrain
  */
-public class Terrain extends BackgroundGameObject {
+public class Terrain extends BackgroundGameObject implements Developable {
     public Terrain(TextureMap texture) {
         super(texture);
     }
